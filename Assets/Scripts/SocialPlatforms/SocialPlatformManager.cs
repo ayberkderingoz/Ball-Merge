4f925c99f2244b9b941f4f760fc676ca6b224e3f
Arda Erbaharlı <ardaerbaharli1@gmail.com>
2024-02-09 19:04:28 +0300

    fixes

--- Assets/Scripts/SocialPlatforms/SocialPlatformManager.cs
+++ Assets/Scripts/SocialPlatforms/SocialPlatformManager.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 
 #if UNITY_ANDROID
+using GooglePlayGames.BasicApi;
 #elif UNITY_IOS
 using UnityEngine.SocialPlatforms.GameCenter;
 #endif
@@ -39,7 +40,12 @@ namespace SocialPlatforms
 #if UNITY_EDITOR
             print("Social platforms are not supported in the editor.");
 #elif UNITY_ANDROID
-            
+              PlayGamesPlatform.Activate();
+            PlayGamesPlatform.Instance.Authenticate((signInStatus) =>
+            {
+                var status = false || signInStatus == SignInStatus.Success;
+                AuthenticateSocialCallBack(status);
+            });
 #elif UNITY_IOS
             Social.localUser.Authenticate(AuthenticateSocialCallBack);
             GameCenterPlatform.ShowDefaultAchievementCompletionBanner(true);
@@ -73,7 +79,7 @@ namespace SocialPlatforms
 #elif UNITY_IOS
 				Social.ShowLeaderboardUI();
 #elif UNITY_ANDROID
-				
+				 PlayGamesPlatform.Instance.ShowLeaderboardUI();
 #endif
         }
 
@@ -85,7 +91,7 @@ namespace SocialPlatforms
 #elif UNITY_IOS
 				Social.ShowAchievementsUI ();
 #elif UNITY_ANDROID
-				
+                PlayGamesPlatform.Instance.ShowAchievementsUI();
 #endif
         }
 
